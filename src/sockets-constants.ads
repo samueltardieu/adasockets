f226faef0225b967f88e955e3d8ba48b00bf6cdb
Samuel Tardieu <sam@rfc1149.net>
2007-11-05 00:12:09 +0100

    Use a non-generated Sockets.Constants package

--- src/sockets-constants.ads
+++ src/sockets-constants.ads
@@ -2,12 +2,11 @@
 --                                                                         --
 --                         ADASOCKETS COMPONENTS                           --
 --                                                                         --
---                               S P L I T                                 --
+--                    S O C K E T S . C O N S T A N T S                    --
 --                                                                         --
---                                B o d y                                  --
+--                                S p e c                                  --
 --                                                                         --
---          Copyright (C) 1998-2007 Samuel Tardieu <sam@rfc1149.net>       --
---               Copyright (C) 1999-2003 ENST http://www.enst.fr/          --
+--            Copyright (C) 2007 Samuel Tardieu <sam@rfc1149.net>          --
 --                                                                         --
 --   AdaSockets is free software; you can  redistribute it and/or modify   --
 --   it  under terms of the GNU  General  Public License as published by   --
@@ -31,62 +30,13 @@
 --   The main repository for this software is located at:                  --
 --       http://www.rfc1149.net/devel/adasockets                           --
 --                                                                         --
---   If you have any question, please send a mail to the AdaSockets list   --
---       adasockets@lists.rfc1149.net                                      --
+--   If you have any question, please send a mail to                       --
+--       Samuel Tardieu <sam@rfc1149.net>                                  --
 --                                                                         --
 -----------------------------------------------------------------------------
 
-with Ada.Command_Line; use Ada.Command_Line;
-with Ada.Text_IO;      use Ada.Text_IO;
+pragma Warnings (Off);
+with GNAT.Sockets.Constants;
+pragma Warnings (On);
 
-procedure Split is
-
-   Column          : Natural  := 0;
-   Max_Column      : constant := 65;
-   Hard_Max_Column : constant := 78;
-   Was_Space       : Boolean  := False;
-
-   procedure Print_Space_Maybe;
-
-   procedure Print_Space_Maybe is
-   begin
-      if Was_Space and then Column /= 0 then
-         Put (' ');
-         Column := Column + 1;
-         Was_Space := False;
-      end if;
-   end Print_Space_Maybe;
-
-begin
-   if Argument_Count /= 1 then
-      Put_Line ("Error, usage: split ""text""");
-      Set_Exit_Status (1);
-   else
-      for I in 1 .. Argument (1) 'Length loop
-         if Argument (1) (I) = ' ' and then Column >= Max_Column then
-            New_Line;
-            Column := 0;
-         elsif Argument (1) (I) = ' ' and then Column = 0 then
-            null;
-         elsif Column >= Hard_Max_Column then
-            New_Line;
-            Put ("--  " & Argument (1) (I));
-            Column := 5;
-         elsif Argument (1) (I) = ' ' then
-            Was_Space := True;
-         else
-            if Column = 0 then
-               Put ("--  ");
-               Column := 4;
-            else
-               Print_Space_Maybe;
-            end if;
-            Put (Argument (1) (I));
-            Column := Column + 1;
-         end if;
-      end loop;
-      if Column > 0 then
-         New_Line;
-      end if;
-   end if;
-end Split;
+package Sockets.Constants renames GNAT.Sockets.Constants;
